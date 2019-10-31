79ba44c9df564fe99af3a11ab7d05e5cad177b23
Student <student@Makerss-MacBook-Pro-10.local>
2019-10-31 13:39:28 +0000

    added functionality to create tables in tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,19 +2,20 @@ import os
 import sys
 topdir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(topdir)
-
-from consumeless import app
+os.environ['APP_SETTINGS']='config.TestingConfig'
+from consumeless import app, db
 
 import unittest
 
-
 class BasicTestCase(unittest.TestCase):
+
+    def setUp(self):
+        db.create_all()
 
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello World!')
+        self.assertEqual(response.status_code, 302)
 
 class AnotherTestCase(unittest.TestCase):
 
