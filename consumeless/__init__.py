ca3be2b377347de862e9f0c4e3a2cfc083032617
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-31 17:28:12 +0000

    attempting to solve CORS

--- consumeless/__init__.py
+++ consumeless/__init__.py
@@ -4,9 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
-
-CORS(app, supports_credentials=True)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
