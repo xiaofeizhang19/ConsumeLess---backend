5dc9ca35d9ef69479b6a31ef650d57294136fcf9
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-11-02 00:15:07 +0000

    Add error handling
    - Add generic `handle_exception` to handle all exceptions we
      are not dealing with elsewhere
    - Add `error` helper to build a JSON error response
    - Return nice error if trying to create a user that already exists
    - Return nice error if logging in with the wrong password

--- consumeless/__init__.py
+++ consumeless/__init__.py
@@ -1,10 +1,22 @@
 import os
 from datetime import date
-from flask import Flask, request, jsonify, redirect, url_for, session
+from flask import (
+    abort,
+    Flask,
+    request,
+    jsonify,
+    redirect,
+    url_for,
+    session,
+    make_response,
+)
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import UniqueViolation
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
@@ -15,22 +27,41 @@ db = SQLAlchemy(app)
 
 from models import Item, User
 
+def error(
+    status=500,
+    message="Internal Server Error"
+):
+    """Build an error response"""
+    return make_response(
+        jsonify(error=message),
+        status,
+    )
+
+
+def handle_exception(e):
+    """Default exception handler
+
+    Any exceptions raised inside a route that we do not explicitly
+    catch will be passed to this function and the we log them
+    server side for debugging and return a generic internal server
+    error to the client.
+    """
+    print(e)
+    return error()
+
+app.handle_exception = handle_exception
+
+
 # using flask-restful to encompass crud for a single route
 class ApiItem(Resource):
     def get(self, i_id):
-        try:
-            item=Item.query.filter_by(id=i_id).first()
-            return jsonify(item.serialize())
-        except Exception as e:
-            return(str(e))
+        item=Item.query.filter_by(id=i_id).first()
+        return jsonify(item.serialize())
 
 class ApiUser(Resource):
     def get(self, u_id):
-        try:
-            user=User.query.filter_by(id=u_id).first()
-            return jsonify(user.serialize())
-        except Exception as e:
-            return(str(e))
+        user=User.query.filter_by(id=u_id).first()
+        return jsonify(user.serialize())
 
 @app.route("/")
 def reroute_index():
@@ -41,16 +72,14 @@ def login_user():
     print(request.form)
     username=request.form.get('username')
     password=request.form.get('password')
-    try:
-        user=User.query.filter_by(username=username).first()
-        if check_password_hash(user.password_hash, password):
-            session.clear()
-            session['user_id'] = user.id
-            return "Well Done"
-        else:
-            return "invalid password"
-    except Exception as e:
-        return(str(e))
+    user=User.query.filter_by(username=username).first()
+
+    if check_password_hash(user.password_hash, password):
+        session.clear()
+        session['user_id'] = user.id
+        return "Well Done"
+
+    abort(error(400, "Invalid password"))
 
 @app.route("/api/item/new", methods=["POST"])
 def add_item():
@@ -62,19 +91,16 @@ def add_item():
     deposit=request.form.get('deposit')
     overdue_charge=request.form.get('overdue_charge')
     created_at=date.today().strftime("%d/%m/%Y")
-    try:
-        item=Item(name = name,
-                description = description,
-                category = category,
-                email = email,
-                deposit = deposit,
-                overdue_charge = overdue_charge,
-                created_at = created_at,)
-        db.session.add(item)
-        db.session.commit()
-        return f"successfully added item: {item.name}"
-    except Exception as e:
-        return(str(e))
+    item=Item(name = name,
+            description = description,
+            category = category,
+            email = email,
+            deposit = deposit,
+            overdue_charge = overdue_charge,
+            created_at = created_at,)
+    db.session.add(item)
+    db.session.commit()
+    return f"successfully added item: {item.name}"
 
 @app.route("/api/item/index")
 def get_all_items():
@@ -96,8 +122,11 @@ def add_user():
         db.session.add(user)
         db.session.commit()
         return f"successfully added user: {user.username}"
-    except Exception as e:
-        return(str(e))
+    except IntegrityError as e:
+        if isinstance(e.orig, UniqueViolation):
+            abort(error(409, "User exists"))
+        else:
+            raise
 
 api.add_resource(ApiItem, '/api/item/<i_id>')
 api.add_resource(ApiUser, '/api/user/<u_id>')
